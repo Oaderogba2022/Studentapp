c63d10ad1e9872ef2abe0695e92e656552469609
Olamide Aderogba <G00401971@atu.ie>
2022-10-18 13:56:33 +0100

    Prompt user for name

--- src/main/java/ie/atu/week5/StudentApp.java
+++ src/main/java/ie/atu/week5/StudentApp.java
@@ -1,4 +1,19 @@
 package ie.atu.week5;
 
+import java.util.Scanner;
+
 public class StudentApp {
+
+    public static void main(String[] args) {
+        System.out.println("Please enter your Student name: ");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("You entered : " + name );
+
+
+
+
+
+    }
+
 }
